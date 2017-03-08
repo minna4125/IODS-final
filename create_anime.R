949eee4a15c3aca3d6c4100c7c7fcb9c3f71372c
Minna Perälampi <minna.peralampi@helsinki.fi>
2017-03-08 16:32:59 +0200

    Final update 3

--- create_anime.R
+++ create_anime.R
@@ -74,10 +74,6 @@
                       
   #Changing the type form character to a factor
   anime_gen$type <- as.factor(anime_gen$type)
-  
-  #Changing genre_cluster into a factor variable
-  a <- factor(anime_gen$genre_cluster)
-  anime_gen$genre_cluster <- a
                     
   #changing episodes from charactor to numeric
   anime_gen$episodes <- as.numeric(anime_gen$episodes)
@@ -95,5 +91,5 @@
                       
   glimpse(anime_gen)
   str(anime_gen)
-  write.csv(anime_gen, "anime_gen2")
+  write.csv(anime_gen, "anime_gen")
   
